8e7e2ca7ef3350157d17fcbe865152bd9281ba14
MaulzzZaqi <madezaqi2006@gmail.com>
2024-09-03 13:37:01 +0700

    pick profile image

--- lib/profile/profile_page.dart
+++ lib/profile/profile_page.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:test_mge_maulana_ade_fathurrozaqi/authentication/login_page.dart';
 
@@ -11,6 +13,8 @@ class _ProfilePageState extends State<ProfilePage> {
   final usernameController = TextEditingController();
   final phoneController = TextEditingController();
   final addressController = TextEditingController();
+  File? profileImage;
+  final ImagePicker imagePicker = ImagePicker();
 
   @override
   void initState() {
@@ -24,6 +28,10 @@ class _ProfilePageState extends State<ProfilePage> {
       usernameController.text = prefs.getString('username') ?? '';
       phoneController.text = prefs.getString('phone') ?? '';
       addressController.text = prefs.getString('address') ?? '';
+      final imagePath = prefs.getString('profileImage');
+      if (imagePath != null) {
+        profileImage = File(imagePath);
+      }
     });
   }
 
@@ -32,6 +40,58 @@ class _ProfilePageState extends State<ProfilePage> {
     await prefs.setString('username', usernameController.text);
     await prefs.setString('phone', phoneController.text);
     await prefs.setString('address', addressController.text);
+    if (profileImage != null) {
+      await prefs.setString('profileImage', profileImage!.path);
+    }
+  }
+
+  Future<void> pickImage(ImageSource source) async {
+    try {
+      final pickedFile = await imagePicker.pickImage(source: source);
+      if (pickedFile != null) {
+        setState(() {
+          profileImage = File(pickedFile.path);
+        });
+        await saveProfileData();
+      } else {
+        print('No image selected.');
+      }
+    } catch (e) {
+      print('Error picking image: $e');
+    }
+  }
+
+  void showImagePickerDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Select Profile Picture'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                pickImage(ImageSource.camera);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Camera'),
+            ),
+            TextButton(
+              onPressed: () {
+                pickImage(ImageSource.gallery);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Gallery'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -42,6 +102,14 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            GestureDetector(
+              onTap: () => showImagePickerDialog(),
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: profileImage != null ? FileImage(profileImage!) : null,
+                child: profileImage == null ? const Icon(Icons.supervisor_account, size: 50) : null,
+              ),
+            ),
             const SizedBox(height: 20),
             Column(
               children: [
